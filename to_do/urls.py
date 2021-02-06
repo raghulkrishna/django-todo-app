fe9d8bab3fe6d6cb1a77390ea0bb61636adb3232
RAGHULKRISHNA <raghulkrishna@KRISH-PRO.local>
2021-02-06 16:58:53 +0100

    todo

--- to_do/urls.py
+++ to_do/urls.py
@@ -15,7 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+path('home/',views.home,name = 'home'),
+    path('current/',views.currenttodos,name = 'currenttodos'),
+path('create/',views.createtodos,name = 'createtodo'),
+path('completed/',views.completedtodos,name = 'completedtodos'),
+
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+
+    #Auth
+    path('signupuser/',views.signupuser,name = 'signupuser'),
+path('logoutuser/',views.logoutuser,name = 'logoutuser'),
+path('loginuser/',views.loginuser,name = 'loginuser')
+
 ]
